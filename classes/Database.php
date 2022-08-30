72b5359d1402737d6bc59ef35f85a4742215670a
Eric Nemchik <eric@nemchik.com>
2022-08-30 22:04:45 +0000

    Add connection configuration manager

--- classes/Database.php
+++ classes/Database.php
@@ -4,13 +4,13 @@ namespace Grav\Plugin\Database;
 
 class Database
 {
-    private $connections = [];
+    private $pdo = [];
 
     public function connect($connect_string, $user='',$pwd='', $options=[])
     {
-        if (!array_key_exists($connect_string, $this->connections)) {
-            $this->connections[$connect_string] = new PDO($connect_string, $user, $pwd, $options);
+        if (!array_key_exists($connect_string, $this->pdo)) {
+            $this->pdo[$connect_string] = new PDO($connect_string, $user, $pwd, $options);
         }
-        return $this->connections[$connect_string];
+        return $this->pdo[$connect_string];
     }
 }
