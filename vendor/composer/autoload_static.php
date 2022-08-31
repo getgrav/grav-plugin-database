dc9e3ca6787913b0103593ac3993181e1cfecb70
Eric Nemchik <eric@nemchik.com>
2022-08-31 17:55:31 +0000

    Add drivers to autoload

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,15 +9,24 @@ class ComposerStaticInitf9be9936aae9e8f353ee7074d1eb9ad4
     public static $prefixLengthsPsr4 = array (
         'G' => 
         array (
+            'Grav\\Plugin\\Database\\Drivers\\' => 29,
             'Grav\\Plugin\\Database\\' => 21,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Grav\\Plugin\\Database\\Drivers\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/classes/drivers',
+        ),
         'Grav\\Plugin\\Database\\' => 
         array (
             0 => __DIR__ . '/../..' . '/classes',
         ),
+    );
+
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -25,6 +34,7 @@ class ComposerStaticInitf9be9936aae9e8f353ee7074d1eb9ad4
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitf9be9936aae9e8f353ee7074d1eb9ad4::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitf9be9936aae9e8f353ee7074d1eb9ad4::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitf9be9936aae9e8f353ee7074d1eb9ad4::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6817c5ecbb6c4772a5be2b6eb8ae7b082ecd9bd5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6817c5ecbb6c4772a5be2b6eb8ae7b082ecd9bd5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
